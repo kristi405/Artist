81da26c3adb5b0fa01e50727c1bd93f8685fc1fb
Kristina <5507151kr@gmail.com>
2020-11-06 16:57:13 +0300

    Initial Commit

---
+++ Artists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artists
+//
+//  Created by kris on 06/11/2020.
+//  Copyright © 2020 kris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
