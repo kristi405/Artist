13b9ad7e54dd03104d2ec430c397390b94e92b4d
Kristina <5507151kr@gmail.com>
2021-04-29 20:59:20 +0300

    Rote bilding

--- Artists/PresentationLayer/ViewController/MapViewController/Controllers/MapEventsViewController.swift
+++ Artists/PresentationLayer/ViewController/MapViewController/Controllers/MapEventsViewController.swift
@@ -11,12 +11,15 @@ final class MapEvents: UIViewController {
         static let widthCancelButton: CGFloat = 25
     }
     
+    @IBOutlet weak var mapView: MKMapView!
+    
     // MARK: Private Properties
     private var locationManager: CLLocationManager?
     private var currentLocation: CLLocation?
-    private var mapView: MKMapView?
     private let mapManager = LocationManager()
     private let annotationIdentifier = "annotationIdentifier"
+    var latitude: Double?
+    var longitude: Double?
     
     // MARK: Public Properties
     
@@ -37,6 +40,23 @@ final class MapEvents: UIViewController {
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+        let sourceLocation = CLLocationCoordinate2D(latitude: 53, longitude: 29)
+        
+        if let latitudeString = event?.venue?.latitude {
+            let latitudeDouble = Double(latitudeString)
+            self.latitude = latitudeDouble
+        }
+        
+        if let longitudeString = event?.venue?.longitude {
+            let longitudeDouble = Double(longitudeString)
+            self.longitude = longitudeDouble
+        }
+        
+        guard let latitude = self.latitude else {return}
+        guard let longitude = self.longitude else {return}
+        let destinationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        showRoutOnMap(pickupCoordinate: sourceLocation, destinationCoordinate: destinationCoordinate)
         
         // Check for Location Services
         if CLLocationManager.locationServicesEnabled() {
@@ -68,6 +88,50 @@ final class MapEvents: UIViewController {
         cancelButton.isHidden = false
         mapView?.addSubview(cancelButton)
     }
+    
+    func showRoutOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
+        let sourcePlacemark = MKPlacemark(coordinate: pickupCoordinate)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationmItem = MKMapItem(placemark: destinationPlacemark)
+        
+        let sourceAnnotation = MKPointAnnotation()
+        
+        if let location = sourcePlacemark.location {
+            sourceAnnotation.coordinate = location.coordinate
+        }
+        
+        let destinationAnnotation = MKPointAnnotation()
+        
+        if let location = destinationPlacemark.location {
+            destinationAnnotation.coordinate = location.coordinate
+        }
+        
+        self.mapView?.showAnnotations([sourceAnnotation, destinationAnnotation], animated: true)
+        
+        let diractionRequest = MKDirections.Request()
+        diractionRequest.source = sourceItem
+        diractionRequest.destination = destinationmItem
+        diractionRequest.transportType = .any
+        
+        let diraction = MKDirections(request: diractionRequest)
+        
+        diraction.calculate { (response, error) in
+            guard let response = response else {
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                return
+            }
+            let route = response.routes[0]
+            
+            self.mapView?.addOverlay((route.polyline), level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.mapView?.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+    }
 }
 
 
@@ -97,10 +161,12 @@ extension MapEvents: CLLocationManagerDelegate, MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        let renderer = MKPolylineRenderer(overlay: overlay)
         renderer.strokeColor = .blue
+        renderer.lineWidth = 5.0
         
         return renderer
     }
+    
 }
 
